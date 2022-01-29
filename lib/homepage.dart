0501bd4509d29bc6681d77418f9377f71095186c
Priyansh_Singhz <ps4151722@gmail.com>
2022-01-30 04:11:40 +0530

    commit

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+import 'dart:math';
+import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,6 +12,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  TextEditingController searchController = new TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,14 +28,31 @@ class _HomePageState extends State<HomePage> {
               margin: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
               decoration: BoxDecoration(
                 color: Colors.black12,
-                borderRadius: BorderRadius.circular(10),
+                borderRadius: BorderRadius.circular(24),
               ),
               child: Row(
                 children: [
-                  GuestureDetector(
-                    onTap: (){}
-                  ),                  Expanded(
+                  GestureDetector(
+                    onTap: () {
+                      if ((searchController.text).replaceAll(" ", "") == "") {
+                        print("Blank search");
+                      } else {
+                        // Navigator.push(context, MaterialPageRoute(builder: (context) => Search(searchController.text)));
+                      }
+                    },
+                    child: Container(
+                      child: Icon(
+                        Icons.search,
+                        color: Colors.blue,
+                      ),
+                      margin: EdgeInsets.fromLTRB(3, 0, 7, 0),
+                    ),
+                  ),
+                  Expanded(
                       child: TextField(
+                    textInputAction: TextInputAction.search,
+                    onSubmitted: (value) {},
+                    controller: searchController,
                     decoration: InputDecoration(
                       border: InputBorder.none,
                       hintText: "Search",
